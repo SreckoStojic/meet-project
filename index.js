130f99b5423d435e94ecc2babb0ab09bfbfc4067
SreckoStojic <stojic.srecko@gmail.com>
2021-09-20 16:54:33 +0200

    Mobile version added.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const btn = document.querySelector(".css-about-btn");
 
 btn.addEventListener("click", function(){
     const el = document.getElementById("secondStepper");
-    el.scrollIntoView(true);
+    el.scrollIntoView({behavior: "smooth"});
 });
 
 
