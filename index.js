85023e90dbe572ec1af32e84dd2e03dbb4224116
SreckoStojic <stojic.srecko@gmail.com>
2021-09-20 09:19:46 +0200

    Footer div changed to footer element. Added js file for scrolling event.

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".css-about-btn");
+
+btn.addEventListener("click", function(){
+    const el = document.getElementById("secondStepper");
+    el.scrollIntoView(true);
+});
+
+
